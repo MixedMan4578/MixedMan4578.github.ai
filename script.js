efb71558daa55c80273a50a33da1b0bbb669e4a2
MixedMan4578 <gavinthompson7845@gmail.com>
2026-01-15 21:43:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let showingNotes = true;
+
+document.addEventListener("keydown", function(e) {
+  if (e.key.toLowerCase() === "n") {
+    showingNotes = !showingNotes;
+    document.getElementById("notes").style.display = showingNotes ? "block" : "none";
+    document.getElementById("games").style.display = showingNotes ? "none" : "block";
+  }
+
+  if (e.key.toLowerCase() === "p") { // Panic key
+    showingNotes = true;
+    document.getElementById("notes").style.display = "block";
+    document.getElementById("games").style.display = "none";
+  }
+});
 
